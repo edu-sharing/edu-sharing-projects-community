966a1e5e273815d1cdfc46a2bfb0a1a552e7e891
Herr Frank Thomschke <thomschke@edu-sharing.net>
2023-10-17 06:09:49 +0000

    Bump SDK

--- deploy/docker/repository/build/service/src/main/build/assets/entrypoint.sh
+++ deploy/docker/repository/build/service/src/main/build/assets/entrypoint.sh
@@ -678,6 +678,9 @@ xmlstarlet ed -L \
 	hocon -f ${eduSConf} set "angular.headers.Content-Security-Policy.style-src" '"'"${my_http_server_csp_style}"'"'
 }
 
+# clean up empty lines in config after hocon commands
+sed -i '/^[[:space:]]*$/d' ${eduSConf}
+
 xmlstarlet ed -L \
   -N x="http://java.sun.com/xml/ns/javaee" \
 	-u '/x:web-app/x:session-config/x:session-timeout' -v "${my_http_server_session_timeout}" \
@@ -694,11 +697,4 @@ done
 
 ########################################################################################################################
 
-# Load libraries
-. /opt/bitnami/scripts/libtomcat.sh
-. /opt/bitnami/scripts/liblog.sh
-
-# Load Tomcat environment variables
-. /opt/bitnami/scripts/tomcat-env.sh
-
-exec "$@"
+exec /opt/bitnami/scripts/tomcat/entrypoint.sh "$@"
